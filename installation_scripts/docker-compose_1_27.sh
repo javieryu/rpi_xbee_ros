45bda63227eac9012ccd4b7abd775f6b2cf6d694
javieryu <javiercyu@gmail.com>
2021-01-05 18:49:24 +0000

    remove deps from docker-compose install sh

--- installation_scripts/docker-compose_1_27.sh
+++ installation_scripts/docker-compose_1_27.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-
-# Install docker-compose dependencies
-DEPS="py-pip python-dev libffi-dev openssl-dev gcc libc-dev make"
-apt update
-apt install $DEPS -y
-
 # Download the current stable release of docker compose
 # in this case 1.27.4
 curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
